cfc5147448da27d243f735e2b56912509b907f6e
Ikhsan Habibi <ikhsanhabibi48@gmail.com>
2019-06-05 16:08:37 +0200

    make default before merge

---
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+}
