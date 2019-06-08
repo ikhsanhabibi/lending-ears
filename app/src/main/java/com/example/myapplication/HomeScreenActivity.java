c48bccec7f9a19e5eb96b82e67b95ff4b274dd78
Ikhsan Habibi <ikhsanhabibi48@gmail.com>
2019-06-09 00:58:02 +0200

    Finished ProfileActivity and HomeScreenActivity

--- app/src/main/java/com/example/myapplication/HomeScreenActivity.java
+++ app/src/main/java/com/example/myapplication/HomeScreenActivity.java
@@ -1,14 +1,19 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
     TextView tellStory;
     TextView listenStory;
+    ImageView profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,20 @@ public class HomeScreenActivity extends AppCompatActivity {
 
         listenStory = findViewById(R.id.listenStory);
         listenStory.setTypeface(typeface);
+
+        // setOnClickListener
+        profile = findViewById(R.id.profile);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("GO", false);
+                startActivity(intent);
+                finish();
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,13 +1,42 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView profileName;
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // Change font
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Montserrat-Regular.ttf");
+
+        profileName = findViewById(R.id.profileName);
+        profileName.setTypeface(typeface);
+
+        // setOnClickListener
+        logo = findViewById(R.id.logo);
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HomeScreenActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("GO", false);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
