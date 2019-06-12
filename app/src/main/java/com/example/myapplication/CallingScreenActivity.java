3bf234a75819ce5745008b91672e31588cecf2d8
Ikhsan Habibi <ikhsanhabibi48@gmail.com>
2019-06-12 14:54:15 +0200

    Add timer Chromometer

--- app/src/main/java/com/example/myapplication/CallingScreenActivity.java
+++ app/src/main/java/com/example/myapplication/CallingScreenActivity.java
@@ -2,12 +2,19 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Chronometer;
 
 public class CallingScreenActivity extends AppCompatActivity {
+
+
+    private Chronometer chronometer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calling_screen);
+
+        chronometer2 = findViewById(R.id.chronometer2);
+        chronometer2.start();
     }
 }
